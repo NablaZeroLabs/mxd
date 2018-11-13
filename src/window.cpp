a0625e6b232becca5bd178ba67a77a18aa74d247
Juan Arrieta <Juan.Arrieta@nablazerolabs.com>
2018-11-12 17:02:30 -0800

    Add Window component

---
+++ src/window.cpp
@@ -0,0 +1,20 @@
+// -*- coding:utf-8; mode:c++; mode:auto-fill; fill-column:80; -*-
+
+/// @file      window.cpp
+/// @brief     Implementation of window.hpp.
+/// @author    J. Arrieta <Juan.Arrieta@nablazerolabs.com>
+/// @date      November 12, 2018
+/// @copyright (C) 2018 Nabla Zero Labs
+
+// Related mxd header
+#include "window.hpp"
+
+// C++ Standard Library
+
+// mxd Library
+
+namespace nzl {
+
+
+
+}  // namespace nzl
---
+++ src/window.hpp
@@ -0,0 +1,18 @@
+// -*- coding:utf-8; mode:c++; mode:auto-fill; fill-column:80; -*-
+
+/// @file      window.hpp
+/// @brief     A Window to display @link Geometries Geometry@endlink.
+/// @author    J. Arrieta <Juan.Arrieta@nablazerolabs.com>
+/// @date      November 12, 2018
+/// @copyright (C) 2018 Nabla Zero Labs
+
+#pragma once
+
+// C++ Standard Library
+
+// mxd Library
+
+namespace nzl {
+
+
+}  // namespace nzl
