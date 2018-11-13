6fd30ec4abee7a0df91f6ee9b6279a0cbb26e864
Juan Arrieta <Juan.Arrieta@nablazerolabs.com>
2018-11-12 17:02:19 -0800

    Add Geometry component

---
+++ src/geometry.hpp
@@ -0,0 +1,18 @@
+// -*- coding:utf-8; mode:c++; mode:auto-fill; fill-column:80; -*-
+
+/// @file      geometry.hpp
+/// @brief     A Geometry is anything that can be drawn.
+/// @author    J. Arrieta <Juan.Arrieta@nablazerolabs.com>
+/// @date      November 12, 2018
+/// @copyright (C) 2018 Nabla Zero Labs
+
+#pragma once
+
+// C++ Standard Library
+
+// mxd Library
+
+namespace nzl {
+
+
+}  // namespace nzl
