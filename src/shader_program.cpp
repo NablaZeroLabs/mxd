44169fc87290408e18902133278322c5943bb28a
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2018-11-27 10:58:03 -0600

    Add shader_program component

---
+++ src/shader_program.cpp
@@ -0,0 +1,20 @@
+// -*- coding:utf-8; mode:c++; mode:auto-fill; fill-column:80; -*-
+
+/// @file      shader_program.cpp
+/// @brief     Implementation of shader_program.hpp.
+/// @author    J. Arrieta <Juan.Arrieta@nablazerolabs.com>
+/// @date      November 27, 2018
+/// @copyright (C) 2018 Nabla Zero Labs
+
+// Related mxd header
+#include "shader_program.hpp"
+
+// C++ Standard Library
+
+// mxd Library
+
+namespace nzl {
+
+
+
+}  // namespace nzl
