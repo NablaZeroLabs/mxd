f8eda98c6432420d7a0f17a420964e4861367ec3
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2018-11-28 14:26:33 -0600

    Implement shader_program header file
    Declared methods needed to link and compile the shader program.

--- src/shader_program.hpp
+++ src/shader_program.hpp
@@ -9,10 +9,30 @@
 #pragma once
 
 // C++ Standard Library
+#include <vector>
 
 // mxd Library
+#include "shader.hpp"
 
 namespace nzl {
+class ShaderProgram {
+ public:
+  /// @brief Create a Shader Program from the given shaders.
+  /// @param shaders Shaders to be used to create Shader Program.
+  ShaderProgram(std::vector<nzl::Shader> shaders) noexcept;
 
+  /// @brief Destroy this Shader Program.
+  ~ShaderProgram() noexcept;
+
+  /// @brief Links and compiles this Shader Program.
+  /// @throws std::runtime_error on compilation failure.
+  void compile();
+
+  /// @brief Return an identifier associated with this Shader Program.
+  unsigned int id() const noexcept;
+ private:
+  const std::vector<nzl::Shader> m_shaders;
+  const unsigned int m_id;
+};
 
 }  // namespace nzl
