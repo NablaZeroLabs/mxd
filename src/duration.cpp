26867cf525eb1dba7c7b983e1f037ebe2f7487e2
Unknown <19davrockenzahn@asfg.edu.mx>
2018-12-03 22:08:07 -0600

    Implement duration operators

--- src/duration.cpp
+++ src/duration.cpp
@@ -35,36 +35,61 @@ Duration Duration::Minutes(double value) noexcept {
   return Duration(value * 60);
 }
 
-Duration Duration::Seconds(double value) noexcept { Duration(value); }
+Duration Duration::Seconds(double value) noexcept { return Duration(value); }
 
 /// @TODO: Implement the rest of the operations (I'm adding some examples).
 
 double Duration::years() const noexcept { return m_value / (86400 * 365.25); }
 
-double Duration::days() const noexcept;
-double Duration::hours() const noexcept;
-double Duration::minutes() const noexcept;
-double Duration::seconds() const noexcept;
+double Duration::days() const noexcept { return m_value / 86400; }
+double Duration::hours() const noexcept { return m_value / 3600; }
+double Duration::minutes() const noexcept { return m_value / 60; }
+double Duration::seconds() const noexcept { return m_value; }
 
-Duration& operator+=(const Duration& other) noexcept {
-  m_duration += other.m_duration;
+Duration& Duration::operator+=(const Duration& other) noexcept {
+  this->m_value += other.m_value;
   return *this;
 }
 
-Duration& operator-=(const Duration& other) noexcept;
-Duration& operator*=(double factor) noexcept;
-Duration& operator/=(double denominator) noexcept;
+Duration& Duration::operator-=(const Duration& other) noexcept {
+  this->m_value -= other.m_value;
+  return *this;
+}
+Duration& Duration::operator*=(double factor) noexcept {
+  this->m_value *= factor;
+  return *this;
+}
+Duration& Duration::operator/=(double denominator) noexcept {
+  this->m_value /= denominator;
+  return *this;
+}
 
 Duration operator+(const Duration& lhs, const Duration& rhs) noexcept {
   auto copy = lhs;
-  lsh += rhs;
+  copy += rhs;
   return copy;
 }
 
-Duration operator-(const Duration& lhs, const Duration& rhs) noexcept;
-Duration operator*(const Duration& lhs, double factor) noexcept;
-Duration operator*(double factor, const Duration& rhs) noexcept;
-Duration operator/(const Duration& lhs, double denominator) noexcept;
+Duration operator-(const Duration& lhs, const Duration& rhs) noexcept {
+  auto copy = lhs;
+  copy -= rhs;
+  return copy;
+}
+Duration operator*(const Duration& lhs, double factor) noexcept {
+  auto copy = lhs;
+  copy *= factor;
+  return copy;
+}
+Duration operator*(double factor, const Duration& rhs) noexcept {
+  auto copy = rhs;
+  copy *= factor;
+  return copy;
+}
+Duration operator/(const Duration& lhs, double denominator) noexcept {
+  auto copy = lhs;
+  copy /= denominator;
+  return copy;
+}
 
 bool operator>(const Duration& lhs, const Duration& rhs) noexcept {
   return lhs.seconds() > rhs.seconds();
