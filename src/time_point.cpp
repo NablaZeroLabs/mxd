8aa612e72be9ace5dcb075968fd444237a96962d
Juan Arrieta <Juan.Arrieta@nablazerolabs.com>
2018-12-08 14:13:57 -0800

    Relocate API declaration

--- src/time_point.cpp
+++ src/time_point.cpp
@@ -14,38 +14,4 @@
 // mxd Library
 #include "duration.hpp"
 
-namespace nzl {
-
-/// @brief A Barycentric Dynamical Time.
-class TimePoint {
- public:
-  /// @brief Build a timepoint from a count of Julian days.
-  static TimePoint Julian(double);
-
-  /// @brief Build a TimePoint.
-  /// @param duration Elapsed duration from the J2000 epoch.
-  TimePoint(Duration duration = Duration()) noexcept;
-
-  /// @brief Return the duration elapsed from the J2000 epoch.
-  Duration elapsed() const noexcept;
-
-  TimePoint& operator+=(const Duration& duration);
-  TimePoint& operator-=(const Duration& duration);
-
- private:
-  /// @note The implementation tracks TDB seconds from the J2000 epoch.
-  Duration m_duration{};
-};
-
-TimePoint operator+(const TimePoint& timepoint, const Duration& duration);
-TimePoint operator-(const TimePoint& timepoint, const Duration& duration);
-
-Duration operator-(const TimePoint& timepoint, const Timepoint& timepoint);
-
-bool operator<(const TimePoint& lhs, const TimePoint& rhs);
-bool operator>(const TimePoint& lhs, const TimePoint& rhs);
-bool operator<=(const TimePoint& lhs, const TimePoint& rhs);
-bool operator>=(const TimePoint& lhs, const TimePoint& rhs);
-bool operator!=(const TimePoint& lhs, const TimePoint& rhs);
-
-}  // namespace nzl
+namespace nzl {}  // namespace nzl
