ef4d3fa8c59bbe3b19798a45a74b1a673d831262
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2018-12-12 10:42:26 -0600

    Add line tests
    glGetError is used in some of the tests to make sure that OpenGL is
    not producing any errors.

--- src/line.t.cpp
+++ src/line.t.cpp
@@ -10,17 +10,102 @@
 #include "line.hpp"
 
 // C++ Standard Library
+#include <vector>
 
 // mxd Library
+#include "mxd.hpp"
+#include "time_point.hpp"
+#include "window.hpp"
 
 // Google Test Framework
 #include <gtest/gtest.h>
 
-TEST( line, Failing ) {
-    ASSERT_TRUE( false ) << "You must add unit tests for line.hpp";
+// Third party libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+TEST(Line, ConstructorAndParameterAccess) {
+  nzl::initialize();
+  nzl::Window win(800, 600, "Test Window");
+  win.hide();
+  win.make_current();
+
+  nzl::Line line(glm::vec3(0.4f, 0.5f, 0.3f));
+
+  EXPECT_FLOAT_EQ(line.color().x, 0.4f);
+  EXPECT_FLOAT_EQ(line.color().y, 0.5f);
+  EXPECT_FLOAT_EQ(line.color().z, 0.3f);
+
+  EXPECT_NE(line.get_program().id(), 0);
+
+  EXPECT_EQ(glGetError(), 0);
+
+  nzl::terminate();
 }
 
-int main( int argc, char** argv ) {
-  ::testing::InitGoogleTest( &argc, argv );
+TEST(Line, OtherConstructors) {
+  nzl::initialize();
+  nzl::Window win(800, 600, "Test Window");
+  win.hide();
+  win.make_current();
+
+  std::vector<glm::vec3> points;
+
+  nzl::Line line1;
+  EXPECT_EQ(glGetError(), 0);
+
+  nzl::Line line2(glm::vec3(1.0f, 1.0f, 1.0f));
+  EXPECT_EQ(glGetError(), 0);
+
+  nzl::Line line3(glm::vec3(1.0f, 0.5f, 0.2f), points);
+  EXPECT_EQ(glGetError(), 0);
+
+  nzl::terminate();
+}
+
+TEST(Line, DrawAndReplacePoints) {
+  nzl::initialize();
+  nzl::Window win(800, 600, "Test Window");
+  win.hide();
+  win.make_current();
+
+  std::vector<glm::vec3> points;
+  points.emplace_back(-1.0f, 1.0f, 0.0f);
+  points.emplace_back(1.0f, -1.0f, 0.0f);
+
+  nzl::Line line(glm::vec3(1.0f, 1.0f, 0.0f), points);
+
+  for (int i = 0; i < 3; i++) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    line.render(nzl::TimePoint());
+
+    win.swap_buffers();
+  }
+  EXPECT_EQ(glGetError(), 0);
+
+  std::vector<glm::vec3> points2;
+  points.emplace_back(-1.0f, -1.0f, 0.0f);
+  points.emplace_back(1.0f, 1.0f, 0.0f);
+
+  line.load_points(points2);
+
+  for (int i = 0; i < 3; i++) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    line.render(nzl::TimePoint());
+
+    win.swap_buffers();
+  }
+  EXPECT_EQ(glGetError(), 0);
+
+  nzl::terminate();
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
