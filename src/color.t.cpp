4581710f002d542371b96b6c2f412e9e2ae9b24b
Juan Arrieta <Juan.Arrieta@nablazerolabs.com>
2018-12-13 10:05:22 -0800

    Run clang-format on all C++ sources

--- src/color.t.cpp
+++ src/color.t.cpp
@@ -16,11 +16,11 @@
 // Google Test Framework
 #include <gtest/gtest.h>
 
-TEST( color, Failing ) {
-    ASSERT_TRUE( false ) << "You must add unit tests for color.hpp";
+TEST(color, Failing) {
+  ASSERT_TRUE(false) << "You must add unit tests for color.hpp";
 }
 
-int main( int argc, char** argv ) {
-  ::testing::InitGoogleTest( &argc, argv );
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
