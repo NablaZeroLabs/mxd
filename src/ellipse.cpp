5849c863a9aed1fc46a1364c932cdb3257a1c26e
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2018-12-13 14:13:35 -0600

    Refactor ellipse class
    The ellipse class has been refactored in order to comply with the
    pimpl idiom and the changes observed in the line class.
    
    The names of the parameters taken have been changed in order to comply
    with project naming standards as well as the astronomical definition
    of an ellipse.

--- src/ellipse.cpp
+++ src/ellipse.cpp
@@ -26,13 +26,12 @@
 #include <glm/glm.hpp>
 
 namespace {  // anonymous namespace
+const std::string vertex_shader_source =
+    nzl::slurp(nzl::get_env_var("MXD_SHADER_ROOT") + "/simple_shader.vert");
+
+const std::string fragment_shader_source =
+    nzl::slurp(nzl::get_env_var("MXD_SHADER_ROOT") + "/simple_shader.frag");
 nzl::Program create_program() {
-  const std::string vertex_shader_source =
-      nzl::slurp(nzl::get_env_var("MXD_SHADER_ROOT") + "/simple_shader.vert");
-
-  const std::string fragment_shader_source =
-      nzl::slurp(nzl::get_env_var("MXD_SHADER_ROOT") + "/simple_shader.frag");
-
   nzl::Shader vert_shader(nzl::Shader::Stage::Vertex, vertex_shader_source);
   vert_shader.compile();
 
@@ -66,79 +65,90 @@ std::vector<glm::vec3> gen_points(float rX, float rY, float number_of_points) {
 
 namespace nzl {
 
-struct Ellipse::IDContainer {
-  glm::vec3 m_color;
-  unsigned int m_vao_id;
-  unsigned int m_vbo_id;
-  int m_number_of_points{0};
-  nzl::Program m_program;
+struct Ellipse::EllipseImp {
+  EllipseImp(float s_ma, float s_mi, float number_of_points);
 
-  IDContainer(glm::vec3 color, float rX, float rY, float number_of_points)
-      : m_color{color}, m_program{create_program()} {
-    glGenVertexArrays(1, &m_vao_id);
-
-    glBindVertexArray(m_vao_id);
-
-    glGenBuffers(1, &m_vbo_id);
-
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_id);
-
-    std::vector<glm::vec3> points{gen_points(rX, rY, number_of_points)};
-
-    // Add the first point again, so the first point is connected to the last.
-    points.push_back(points.front());
-
-    m_number_of_points = points.size();
-
-    glBufferData(GL_ARRAY_BUFFER, points.size() * 3 * sizeof(float),
-                 points.data(), GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void*)0);
-
-    glDisableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
-  }
-
-  ~IDContainer() {
-    glDeleteVertexArrays(1, &m_vao_id);
-    glDeleteBuffers(1, &m_vbo_id);
-  }
+  ~EllipseImp();
+  nzl::Program program;
+  glm::vec3 color;
+  unsigned int vao_id;
+  unsigned int vbo_id;
+  int number_of_points{0};
 };
 
-Ellipse::Ellipse(float rX, float rY, int number_of_points,
-                 glm::vec3 color) noexcept
-    : m_id_container{
-          std::make_shared<IDContainer>(color, rX, rY, number_of_points)} {}
+nzl::Ellipse::EllipseImp::EllipseImp(float s_ma, float s_mi,
+                                     float number_of_points)
+    : program{create_program()} {
+  glGenVertexArrays(1, &vao_id);
 
-glm::vec3 Ellipse::color() const noexcept { return m_id_container->m_color; }
+  glBindVertexArray(vao_id);
 
-void Ellipse::set_color(glm::vec3 color) noexcept {
-  m_id_container->m_color = color;
-}
+  glGenBuffers(1, &vbo_id);
 
-nzl::Program Ellipse::get_program() const noexcept {
-  return m_id_container->m_program;
-}
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
 
-void Ellipse::do_render(TimePoint t [[maybe_unused]]) {
-  m_id_container->m_program.use();
-  m_id_container->m_program.set("color", m_id_container->m_color);
+  std::vector<glm::vec3> points{gen_points(s_ma, s_mi, number_of_points)};
 
-  glBindVertexArray(m_id_container->m_vao_id);
+  // Add the first point again, so the first point is connected to the last
+
+  points.push_back(points.front());
+
+  number_of_points = points.size();
+
+  glBufferData(GL_ARRAY_BUFFER, points.size() * 3 * sizeof(float),
+               points.data(), GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
 
-  glDrawArrays(GL_LINE_STRIP, 0, m_id_container->m_number_of_points);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+  glDisableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
+
+  nzl::check_gl_errors();
+}
+
+nzl::Ellipse::EllipseImp::~EllipseImp() {
+  glDeleteVertexArrays(1, &vao_id);
+  glDeleteBuffers(1, &vbo_id);
+  nzl::check_gl_errors();
+}
+
+// -----------------------------------------------------------------------------
+//         The section below forwards API calls to the implementation
+// -----------------------------------------------------------------------------
+
+Ellipse::Ellipse(float s_ma, float s_mi, int number_of_points, glm::vec3 color)
+    : m_pimpl{std::make_shared<EllipseImp>(s_ma, s_mi, number_of_points)} {
+  m_pimpl->color = color;
+}
+
+glm::vec3 Ellipse::color() const noexcept { return m_pimpl->color; }
+
+void Ellipse::set_color(glm::vec3 color) noexcept { m_pimpl->color = color; }
+
+const nzl::Program& Ellipse::get_program() const noexcept {
+  return m_pimpl->program;
+}
+
+void Ellipse::do_render(TimePoint t [[maybe_unused]]) {
+  m_pimpl->program.use();
+  m_pimpl->program.set("color", m_pimpl->color);
+
+  glBindVertexArray(m_pimpl->vao_id);
+
+  glEnableVertexAttribArray(0);
+
+  glDrawArrays(GL_LINE_STRIP, 0, m_pimpl->number_of_points);
 
   glDisableVertexAttribArray(0);
 
   glBindVertexArray(0);
+
+  nzl::check_gl_errors();
 }
 
 }  // namespace nzl
