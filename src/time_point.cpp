a19f5885ff29fdcc2f2f1977941896a1d5a4ec00
Juan Arrieta <Juan.Arrieta@nablazerolabs.com>
2018-12-13 07:04:34 -0800

    Fix implementation of TimePoint::Julian

--- src/time_point.cpp
+++ src/time_point.cpp
@@ -16,21 +16,23 @@
 #include "duration.hpp"
 
 namespace nzl {
-  
+
 TimePoint TimePoint::Julian(double days) {
-  return TimePoint{Duration::Days(days)};
+  // The implementation of TimePoint tracks the number of seconds from the J2000
+  // epoch (01/Jan/2000 12:00:00.00), which has Julian Date 2451545.0.
+  const auto J2000 = 2451545.0;
+  return TimePoint{Duration::Days(days - J2000)};
 }
 
 TimePoint TimePoint::DistantPast() noexcept {
-  return TimePoint(
-      Duration::Seconds(std::numeric_limits<double>::infinity() * -1));
+  return TimePoint(Duration::Seconds(-std::numeric_limits<double>::infinity()));
 }
 
 TimePoint TimePoint::DistantFuture() noexcept {
   return TimePoint(Duration::Seconds(std::numeric_limits<double>::infinity()));
 }
 
-TimePoint::TimePoint(Duration duration) noexcept { m_duration = duration; }
+TimePoint::TimePoint(Duration duration) noexcept : m_duration{duration} {}
 
 Duration TimePoint::elapsed() const noexcept { return m_duration; }
 
